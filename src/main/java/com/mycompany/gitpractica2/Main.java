6ab6e3043b63de828341d8d6b4bff318ac7adce3
jose ramon gil <jose ramon gil>
2016-03-17 19:45:00 +0100

    primer commit

---
+++ src/main/java/com/mycompany/gitpractica2/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gitpractica2;
+
+/**
+ *
+ * @author JOSE RAMON
+ */
+public class Main {
+    public static void main(String[] args) {
+          System.out.println("Bienvenidos al mantenimiento, dos");
+      for (int i=0; i<10;i++){
+         System.out.print(i+",");
+      }
+    }
+}
